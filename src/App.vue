17de04f006aa30899a1d1bb7f4043ed3e665c34b
Nicolas <ncorantehuenchuman@gmail.com>
2025-01-09 21:46:50 -0300

    Primer Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+</style>

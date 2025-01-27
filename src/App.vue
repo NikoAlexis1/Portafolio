2dee91f53042f97cc62c2debce88a3274bf126af
Nicolas <ncorantehuenchuman@gmail.com>
2025-01-27 17:02:54 -0300

    Modificacion archivo app.vue, creacion aboutme

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
 import HeroSection from './components/HeroSection.vue'
+import Aboutme from './components/About.vue'
 </script>
 
 <template>
   <header>
-    <Navbar />
+    <nav class="navbar navbar-expand-sm bg-dark">
+        <div class="container-fluid d-flex justify-content-end">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#hero">Home</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#about">About me</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
   </header>
 
   <main>
-    <HeroSection />
+    <div class="hero" id="hero">
+      <HeroSection />
+    </div>
+    <div class="about" id="about" >
+      <Aboutme />
+    </div>
+    
+    
   </main>
 </template>
 
 <style scoped>
+.about {
+  background-color: #000000;
+}
+.navbar {
+    position: fixed;
+    width: 100%;
+}
+.navbar-toggler{
+    background-color: #e0e0e0;
+}
+
+.navbar-nav{
+    margin: 0 auto;
+}
+
+.nav-link {
+    color: #c3c3c3;
+}
+
+.nav-link:hover {
+    color: #ffffff;
+}
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>About me</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa cumque quam praesentium expedita ad quos aliquam quis dicta repellendus itaque eligendi aperiam, fugiat facere, ex vero maiores ratione! Ipsa illum quas, fugit totam minima veniam vero voluptas consequuntur delectus! Porro quia accusantium nihil non? Aperiam quibusdam dolorem itaque nulla beatae accusamus, non cumque explicabo atque distinctio autem porro odio voluptas.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+div {
+    color: #e1e1e1;
+    padding: 20px 0;
+}
+h1{
+    margin-bottom: 10px;
+}
+p, h1{
+    margin: 0 100px;
+    text-align: center;
+}
+</style>
